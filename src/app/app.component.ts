19beda33463160d8aa961a4731848b28f5b932ed
SergioG <sl_garcia@yahoo.com.ar>
2024-07-10 14:58:59 -0300

    n5-eventos-metodos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto-prefinal';
+  mostrarSaludo: boolean = true;
+
+  ocultarSaludo() {
+    this.mostrarSaludo = false;}
+
 }
--- src/app/saludo/saludo.component.ts
+++ src/app/saludo/saludo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input  } from '@angular/core';
 
 @Component({
   selector: 'app-saludo',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./saludo.component.css']
 })
 export class SaludoComponent {
+
+  @Input() mostrarSaludo: boolean = true;
+  nombre: string = 'Sergio Garcia';
 
 }
